c7d80cf39c8e7117468b40f46a65b459cbbc78e2
Arystan <arystankaliakparov@gmail.com>
2024-09-02 23:37:00 +0500

    fix text

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            New text new new new 2 5
+            New text new new new 2 5 4
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
